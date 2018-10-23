6d78dbb844672b6a07931a7ff77e55983e237bcb
Mmarcopollo <szafranmarek97@gmail.com>
2018-10-23 13:07:41 +0200

    Add reflector

---
+++ Project.WPF/Model/Reflector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TPA.Reflection.Model;
+
+namespace Project.WPF.Model
+{
+    class Reflector
+    {
+        public Reflector(string assemblyFile)
+        {
+            if (string.IsNullOrEmpty(assemblyFile))
+                throw new System.ArgumentNullException();
+            Assembly assembly = Assembly.LoadFrom(assemblyFile);
+            m_AssemblyModel = new AssemblyMetadata(assembly);
+        }
+        public Reflector(Assembly assembly)
+        {
+            m_AssemblyModel = new AssemblyMetadata(assembly);
+        }
+        internal AssemblyMetadata m_AssemblyModel { get; private set; }
+    }
+}
