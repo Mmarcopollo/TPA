7684c6a96f0de5a27ff0263a41586c09390fde09
Mmarcopollo <szafranmarek97@gmail.com>
2018-11-07 22:47:05 +0100

    More green dots

--- WPFTestProject/ReflectionTest.cs
+++ WPFTestProject/ReflectionTest.cs
@@ -39,6 +39,74 @@ namespace WPFTestProject
 
         }
 
+        [TestMethod]
+        public void NamespacemetadataTest_IsNumberOfElementsEqualToRealNumber()
+        {
+            string path = "..\\..\\..\\MyLibrary\\bin\\Debug\\MyLibrary.dll";
+            Reflector reflector = new Reflector(path);
+
+            List<NamespaceMetadata> namespaces = reflector.M_AssemblyModel.Namespaces.ToList();
+            Assert.AreEqual(namespaces.Count, 1);
+
+        }
+
+        [TestMethod]
+        public void TypesMetadataTest_IsNumberOfElementsEqualToRealNumber()
+        {
+            string path = "..\\..\\..\\MyLibrary\\bin\\Debug\\MyLibrary.dll";
+            Reflector reflector = new Reflector(path);
+
+            List<NamespaceMetadata> namespaces = reflector.M_AssemblyModel.Namespaces.ToList();
+            List<TypeMetadata> types = namespaces[0].m_Types.ToList();
+            Assert.AreEqual(types.Count, 3);
+
+        }
+
+        [TestMethod]
+        public void MethodsMetadataTest_IsNumberOfElementsEqualToRealNumber()
+        {
+            string path = "..\\..\\..\\MyLibrary\\bin\\Debug\\MyLibrary.dll";
+            Reflector reflector = new Reflector(path);
+
+            List<NamespaceMetadata> namespaces = reflector.M_AssemblyModel.Namespaces.ToList();
+            Assert.AreEqual(namespaces.Count, 1);
+            List<TypeMetadata> types = namespaces[0].m_Types.ToList();
+            Assert.AreEqual(types.Count, 3);
+            List<MethodMetadata> methods;
+
+            methods = types[0].m_Methods.ToList();
+            Assert.AreEqual(methods.Count, 8);
+
+            methods = types[1].m_Methods.ToList();
+            Assert.AreEqual(methods.Count, 8);
+
+            methods = types[2].m_Methods.ToList();
+            Assert.AreEqual(methods.Count, 9);
+
+        }
+
+        [TestMethod]
+        public void PropertiesMetadataTest_IsNumberOfElementsEqualToRealNumber()
+        {
+            string path = "..\\..\\..\\MyLibrary\\bin\\Debug\\MyLibrary.dll";
+            Reflector reflector = new Reflector(path);
+
+            List<NamespaceMetadata> namespaces = reflector.M_AssemblyModel.Namespaces.ToList();
+            List<TypeMetadata> types = namespaces[0].m_Types.ToList();
+ 
+            List<PropertyMetadata> props;
+
+            props = types[0].m_Properties.ToList();
+            Assert.AreEqual(props.Count, 2);
+
+            props = types[1].m_Properties.ToList();
+            Assert.AreEqual(props.Count, 2);
+
+            props = types[2].m_Properties.ToList();
+            Assert.AreEqual(props.Count, 2);
+
+        }
+
 
     }
 }
