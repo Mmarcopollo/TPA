72405bb5331bb82d34708f8dea3c14d7fcdbc727
PM <pablo2231@o2.pl>
2018-11-07 13:59:16 +0100

    Added function that displays tree on console. It's still needed to implement building that tree. Note that logs from console don't create themselves.

--- ViewConsole/Program.cs
+++ ViewConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,30 @@ namespace ViewConsole
                 if (!isSuccessfullyRead) Console.WriteLine("You typed wrong path. Try again.");
                 Console.Write("Write the path to file you want load:");
                 string path = Console.ReadLine();
-                viewModel.PathVariable = path;
+                //viewModel.PathVariable = path;
+                viewModel.PathVariable = "C:\\Users\\Ola\\Desktop\\TPA\\TPA\\MyLibrary\\bin\\Debug\\MyLibrary.dll";
                 isSuccessfullyRead = viewModel.LoadDLL();
             }
             while (!isSuccessfullyRead);
 
+            foreach(TreeViewNode node in viewModel.HierarchicalAreas)
+            {
+                DisplayTree(node, 0);
+            }
+
+        }
+
+        private static void DisplayTree(TreeViewNode node, int level)
+        {
+            if(node != null)
+            { 
+                for (int i = 0; i < level; i++) Console.WriteLine("    ");
+                Console.WriteLine(node.FullName + "\n");
+                foreach (TreeViewNode childNode in node.Children)
+                {
+                    DisplayTree(childNode, level + 1);
+                }
+            }
         }
     }
 }
