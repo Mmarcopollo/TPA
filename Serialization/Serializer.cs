36edb9b407afbd6f3530b82eb257eca280be6267
Mmarcopollo <szafranmarek97@gmail.com>
2018-11-28 18:51:20 +0100

    Aded tests

--- Serialization/Serializer.cs
+++ Serialization/Serializer.cs
@@ -7,19 +7,31 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using log4net;
 
 namespace Serialization
 {
+    [KnownType(typeof(log4net.Util.TypeConverters.TypeConverterAttribute))]
     public class Serializer : ISerializer
     {
         public void Write(AssemblyMetadata obj, string filePath)
         {
-            DataContractSerializer serializer = new DataContractSerializer(obj.GetType());
+            var lista = new List<Type>();
+            lista.Add(typeof(System.FlagsAttribute));
+            lista.Add(typeof(System.Reflection.DefaultMemberAttribute));
+            lista.Add(typeof(System.AttributeUsageAttribute));
+            lista.Add(typeof(System.ObsoleteAttribute));
+            lista.Add(typeof(System.SerializableAttribute));
+            lista.Add(typeof(log4net.Util.TypeConverters.TypeConverterAttribute));
+            
+
+           DataContractSerializer serializer = new DataContractSerializer(obj.GetType(), lista);
 
             using (FileStream stream = File.Create(filePath))
             {
                 serializer.WriteObject(stream, obj);
             }
+
         }
 
         public AssemblyMetadata Read(string filePath)
---
+++ WPFTestProject/SerializationTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using Moq;
+using Serialization;
+using ViewModel;
+
+namespace WPFTestProject
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        private string pathDLL = "..\\..\\..\\MyLibrary\\bin\\Debug\\MyLibrary.dll";
+        private string pathXML = "..\\..\\..\\MyLibrary\\bin\\Debug\\Data.xml";
+        [TestMethod]
+        public void Seralizer_CheckExistanceOfFile()
+        {
+
+            Reflector reflector = new Reflector(pathDLL);
+
+            Serializer repozitory = new Serializer();
+            repozitory.Write(reflector.M_AssemblyModel, pathXML);
+
+            AssemblyMetadata assemblyLoaded = repozitory.Read(pathXML);
+
+            Assert.IsTrue(File.Exists("..\\..\\..\\MyLibrary\\bin\\Debug\\Data.xml"));
+        }
+        [TestMethod]
+        public void Seralizer_LoadFile()
+        {
+
+            Reflector reflector = new Reflector(pathDLL);
+
+            Serializer repozitory = new Serializer();
+            repozitory.Write(reflector.M_AssemblyModel, pathXML);
+
+            AssemblyMetadata assemblyLoaded = repozitory.Read(pathXML);
+
+            Assert.AreEqual(reflector.M_AssemblyModel.m_Name,assemblyLoaded.m_Name);
+        }
+    }
+}
