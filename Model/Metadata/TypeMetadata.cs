e8673f3096037d7fd1c088d977e99ebd73da6fd2
Mmarcopollo <szafranmarek97@gmail.com>
2019-01-16 17:43:23 +0100

    Add references

--- Model/Metadata/TypeMetadata.cs
+++ Model/Metadata/TypeMetadata.cs
@@ -6,9 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Model
 {
