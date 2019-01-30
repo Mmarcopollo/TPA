d9c727b33597ad6cf3e05181e915aa326ead86af
Mmarcopollo <szafranmarek97@gmail.com>
2019-01-30 20:39:05 +0100

    Add atributes to fields

--- ViewModel/Treeview/FieldTreeView.cs
+++ ViewModel/Treeview/FieldTreeView.cs
@@ -21,6 +21,13 @@ namespace ViewModel.Treeview
 
         public override void BuildMyself(ObservableCollection<TreeViewNode> children)
         {
+            if (_field.Attributes != null) //Attributes
+            {
+                foreach (var attribute in _field.Attributes)
+                {
+                    Children.Add(new AttributeTreeView(attribute));
+                }
+            }
 
             if (_field.FieldType != null) // Type
             {
